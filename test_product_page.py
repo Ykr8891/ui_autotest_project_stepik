3a665139540d180489bb2033c0ac42b6383b5018
IuriiKrasnov <iurii.krasnov@etnatrader.com>
2022-12-27 22:55:25 +0300

    add base_page.py go_to_login_page and should_be_login_link methods add test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page tests

--- test_product_page.py
+++ test_product_page.py
@@ -2,34 +2,35 @@ import time
 
 import pytest
 
+from pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   pytest.param(
-#                                       "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-#                                       marks=pytest.mark.xfail(reason="Incorrect alert message")
-#                                   ),
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link):
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_product_to_basket()
-#     page.solve_quiz_and_get_code()
-#     main_product_name = page.get_main_product_name()
-#     main_product_price = page.get_main_product_price()
-#     alert_product_name = page.get_alert_product_name()
-#     alert_product_price = page.get_alert_product_price()
-#     assert main_product_name == alert_product_name, f'Main page name {main_product_name} is nor equal to alert name {alert_product_name}'
-#     assert main_product_price == alert_product_price, f'Main page price {main_product_price} is nor equal to alert price {alert_product_price}'
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail(reason="Incorrect alert message")
+                                  ),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    main_product_name = page.get_main_product_name()
+    main_product_price = page.get_main_product_price()
+    alert_product_name = page.get_alert_product_name()
+    alert_product_price = page.get_alert_product_price()
+    assert main_product_name == alert_product_name, f'Main page name {main_product_name} is nor equal to alert name {alert_product_name}'
+    assert main_product_price == alert_product_price, f'Main page price {main_product_price} is nor equal to alert price {alert_product_price}'
 
 
 @pytest.mark.xfail(reason='Broken logic')
@@ -57,3 +58,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     assert page.is_disappeared(*ProductPageLocators.ALERT_PRODUCT_NAME), "Success message hasn't disappeared after adding product to basket"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
